8d426146b390fe9d594e63a87ff4132f55ed7102
William <webuntermench@protonmail.com>
2023-09-21 21:23:51 +0300

    Updated component FilmSeats

--- src/components/FilmSeats.vue
+++ src/components/FilmSeats.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
 import type { FilmSession } from '@/listFilms'
+import SeatsOrder from './SeatsOrder.vue'
+import { ref } from 'vue'
 const props = defineProps<{
   seats: FilmSession
   date: Date
 }>()
 const isReserved = (seat: number) => props.seats.reserved.includes(seat)
+
+const reserveSeats = ref<number[]>([])
+
+const seatsWrapper = ref<HTMLElement | null>(null)
+
+const RESERVED_NAME = 'faq__reserved'
+
+const clickHandler = (seat: number, event: MouseEvent) => {
+  const element = event.target as HTMLDivElement
+  if (element.classList.contains(RESERVED_NAME)) {
+    //TODO
+    const index = reserveSeats.value.indexOf(seat)
+    reserveSeats.value.splice(index, 1)
+    element.classList.remove(RESERVED_NAME)
+    return
+  }
+  reserveSeats.value.push(seat)
+  element.classList.add('faq__reserved')
+}
+
+const removeHandler = (id: number) => {
+  //TODO
+  const index = reserveSeats.value.indexOf(id)
+  reserveSeats.value.splice(index, 1)
+  seatsWrapper.value?.children[id - 1].classList.remove(RESERVED_NAME)
+}
 </script>
 <template>
   <section class="seats__wrapper">
@@ -14,15 +42,18 @@ const isReserved = (seat: number) => props.seats.reserved.includes(seat)
       <div>{{ props.date.toLocaleDateString() }}</div>
     </section>
     <div class="chairs__wrapper">
-      <div class="seats__chairs">
+      <div class="seats__chairs" ref="seatsWrapper">
         <div
+          type="button"
           class="seats__chair"
           v-for="chair in props.seats.totalSeats"
           :key="chair"
-          :class="isReserved(chair) ? `faq__circle_red` : `faq__circle_blue`"
+          :class="isReserved(chair) ? `faq__circle_red` : ``"
           v-tooltip.top="chair"
+          @click="clickHandler(chair, $event)"
         ></div>
       </div>
+      <SeatsOrder :seats="reserveSeats" @remove-order="removeHandler" />
     </div>
     <section class="seats__faq">
       <div class="faq__circle faq__circle_blue"></div>
@@ -58,7 +89,8 @@ const isReserved = (seat: number) => props.seats.reserved.includes(seat)
 }
 
 .chairs__wrapper {
-  display: flex;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
   justify-content: center;
   align-items: center;
   margin-top: 5rem;
@@ -72,17 +104,11 @@ const isReserved = (seat: number) => props.seats.reserved.includes(seat)
   margin-bottom: 0.5rem;
 }
 
-.seats__chairs__middle {
-  grid-template-columns: repeat(10, 1rem);
-}
-.seats__chairs__top {
-  grid-template-columns: repeat(8, 1rem);
-  margin-bottom: 0.5rem;
-}
 .seats__chair {
   padding: 1rem;
   border-radius: 0.175rem;
   cursor: pointer;
+  background-color: rgb(125, 177, 229);
   box-shadow:
     rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
@@ -108,7 +134,12 @@ const isReserved = (seat: number) => props.seats.reserved.includes(seat)
 .faq__circle_blue {
   background-color: rgb(125, 177, 229);
 }
+.faq__reserved {
+  background-color: rgb(255, 170, 0);
+}
 .faq__circle_red {
   background-color: rgba(128, 128, 128, 0.5);
+  cursor: not-allowed;
+  pointer-events: none;
 }
 </style>
