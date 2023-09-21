1c3d5dada2db96dae4e601fcb931f8a4f4c75075
William <webuntermench@protonmail.com>
2023-09-21 21:34:54 +0300

    Added new test for header

---
+++ src/components/ui/AppHeader.spec.ts
@@ -0,0 +1,26 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import AppHeader from './AppHeader.vue'
+
+document.body.innerHTML = `
+<div id="app" style="height:10000px">
+</div>`
+
+describe('test AppHeader', () => {
+  it('should render', () => {
+    const wrapper = mount(AppHeader)
+    expect(wrapper.find('h2').text()).toBe('My Header')
+  })
+  it("component's class 'sticky' is false", () => {
+    const wrapper = mount(AppHeader)
+    expect(wrapper.classes()).not.toContain('sticky')
+  })
+  it('imitate scroll', async () => {
+    const wrapper = mount(AppHeader, {
+      attachTo: document.getElementById('app') as HTMLElement
+    })
+    window.dispatchEvent(new CustomEvent('scroll', { detail: 200 }))
+    await wrapper.trigger('scroll')
+    console.log(wrapper.classes())
+  })
+})
