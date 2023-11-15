7c55d18613e8c445413be26016714f389987ef74
William <webuntermench@protonmail.com>
2023-11-15 10:32:55 +0300

    Added nesting css

--- src/components/SeatsChairs.vue
+++ src/components/SeatsChairs.vue
@@ -37,32 +37,36 @@ const clickHandler = (chair: number) => {
 </template>
 
 <style scoped>
-.seats__chairs {
-  border-radius: 0.5rem;
-  display: grid;
-  gap: 1.5rem;
-  justify-content: center;
-  grid-template-columns: repeat(8, 1rem);
-  margin-bottom: 0.5rem;
+.seats {
+  &__chairs {
+    border-radius: 0.5rem;
+    display: grid;
+    gap: 1.5rem;
+    justify-content: center;
+    grid-template-columns: repeat(8, 1rem);
+    margin-bottom: 0.5rem;
+  }
+
+  &__chair {
+    padding: 1rem;
+    border-radius: 0.175rem;
+    cursor: pointer;
+    background-color: var(--seat-free);
+    box-shadow:
+      rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+      rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+  }
 }
 
-.seats__chair {
-  padding: 1rem;
-  border-radius: 0.175rem;
-  cursor: pointer;
-  background-color: var(--seat-free);
-  box-shadow:
-    rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-}
+.faq {
+  &__reserved {
+    background-color: var(--seat-reserved-client);
+  }
 
-.faq__reserved {
-  background-color: var(--seat-reserved-client);
-}
-
-.faq__circle_red {
-  background-color: var(--seat-reserved-other);
-  cursor: not-allowed;
-  pointer-events: none;
+  &__circle_red {
+    background-color: var(--seat-reserved-other);
+    cursor: not-allowed;
+    pointer-events: none;
+  }
 }
 </style>
